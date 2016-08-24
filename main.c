ae589bec821b212c70de2671b1a60e92f144857a
charlesduarte019 <charlesduarte019@hotmail.com>
2016-08-24 17:51:43 -0300

    First Commit

---
+++ main.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.c
+ * Author: charl
+ *
+ * Created on 24 de Agosto de 2016, 17:38
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("Hello World\n");
+    printf("c019.eit.br\n");
+    
+    return (EXIT_SUCCESS);
+}
+
